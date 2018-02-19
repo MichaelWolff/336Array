b89bc1f294ebc314b98530426d6fcb6bfd5cd1bc
Michael Wolff <mwolff@csumb.edu>
2018-02-19 17:18:50 +0000

    working on the base index file

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+include 'inc/functions.php';
+?>
+<!DOCTYPE html>
+<!--The function of this assignment will be to calculate numbers-->
+<html>
+    
+    <head>
+        <title> SILVERJACK </title>
+        <style>
+            /*@import url("css/styles.css");*/
+        </style>
+        
+        CALCULATOR
+    </head>
+    <body>
+        <div id="main">
+        <!--<img src="img/cherry.png" alt="cherry" title="Cherry" width="70" />-->
+        <?php
+        $playerList = array("");
+        $cards = array("");
+        ?>
+        <form>
+            
+            <input type="checkbox" name="replay" value="Replay"> Replay 
+
+
+        </form>
+        </div>
+    </body>
+</html>
